958f658562453ecd3f7997d6e7df351589ec659f
ZoniBoy00 <34036787+ZoniBoy00@users.noreply.github.com>
2024-09-21 09:11:58 +0300

    Update sv_rob.lua

--- server/sv_rob.lua
+++ server/sv_rob.lua
@@ -1,8 +1,11 @@
 Citizen.CreateThread(function()
+    local resourceName = 'P_playerRob'
+
     while true do
-        if GetCurrentResourceName() ~= 'P_playerRob' then
-            print('Change resource ' .. GetCurrentResourceName() .. ' name to P_playerRob')
+        local currentResource = GetCurrentResourceName()
+        if currentResource ~= resourceName then
+            print('Change resource name to ' .. resourceName)
         end
-        Citizen.Wait(60000)
+        Citizen.Wait(60000) -- Check every minute
     end
 end)
